ecd6df5f80bad9b1b0876f62bc98f55b299d4faa
OGabrieLima <mrlima.dev@gmail.com>
2023-12-27 10:38:06 -0300

    Installing packages and configuring ZOD

---
+++ app/src/llib/zod/index.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  
+  /** Riot Games */
+  RIOT_TOKEN: z.string(),
+  // Endpoints
+  ENDPOINT_MATCH: z.string(),
+  ENDPOINT_SUMMONER: z.string(),
+  ENDPOINT_RIOT_ACCOUNT: z.string()
+
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if(! _env.success )
+{
+  console.error(`[${new Date().toLocaleString().replace(',', ' |')}] 🟥 Invalid environment variable! ${_env.error.format()}`);
+  throw new Error(`[${new Date().toLocaleString().replace(',', ' |')}] 🟥 Invalid environment variable! ${_env.error.format()}`);
+}
+
+export const env = _env.data;
